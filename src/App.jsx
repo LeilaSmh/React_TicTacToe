da615b8fc6ee618f5fbc971303e5fd133a7bd298
LeilaSmh <leilasamih.ls@gmail.com>
2024-01-18 16:04:11 +0100

    handle active player and lift state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayerSymbol, setActivePlayerSymbol] = useState("X");
+
+  const handleSquareSelect = () => {
+    setActivePlayerSymbol((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  };
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            name="Player 1"
+            symbol="X"
+            isActive={activePlayerSymbol === "X"}
+          />
+          <Player
+            name="Player 2"
+            symbol="O"
+            isActive={activePlayerSymbol === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          activePlayerSymbol={activePlayerSymbol}
+          onSquareSelect={handleSquareSelect}
+        />
       </div>
     </main>
   );
