fe4ca1922dbea7b33cc8b55908e9ac1be2db7501
LeilaSmh <leilasamih.ls@gmail.com>
2024-01-18 15:46:42 +0100

    Game board UI

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const INTITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(INTITIAL_GAME_BOARD);
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
